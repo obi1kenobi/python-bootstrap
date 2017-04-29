6282745ce33e357c952ec5bdc0c80ed81b10aa1b
Predrag Gruevski <predrag.gruevski@gmail.com>
2017-04-29 10:26:06 -0400

    Add setup.py to make the package installable.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+package_name = 'example_package'
+
+setup(name=package_name,
+      version='0.0.1',
+      packages=find_packages(),
+      install_requires=[],
+      )
